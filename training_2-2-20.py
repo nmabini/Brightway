7405eb5c43a61c31bcf9e5841e88695585c1f7da
nmabini <46689271+nmabini@users.noreply.github.com>
2020-02-03 08:49:34 -0800

    Add files via upload

--- training_2-2-20.py
+++ training_2-2-20.py
@@ -5,6 +5,13 @@ import matplotlib.pyplot as plt
 import random
 from slider import Slider
 
+import dask
+from dask import delayed
+#from dask.distributed import Client
+#client = Client() # start local client
+#from dask import delayed, compute
+dask.config.set(scheduler='threads')
+
 from binaryclassifier import BinaryClassifier
 from sklearn.svm import LinearSVC
 from sklearn.preprocessing import StandardScaler
@@ -19,7 +26,16 @@ exFeatures_arr=[]
 
 once = 0
 # array to hold boxes to draw on image
-boxes = []
+
+# function to apply to each chunk
+def predictFunc(x, y, window, bc, boxes):
+    ex_features, ex_hog_image = hog(window, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True, multichannel = True)
+    arr = ex_features.reshape(1, -1)
+    if(arr.shape == (1, 128)):
+        if bc.predict(arr):
+            conf = bc.conf(arr)
+            boxes.append((x, y, window, conf))
+    return 1
 
 posImgs = "./p"
 negImgs = "./n"
@@ -54,8 +70,6 @@ print("Feature Extraction time: ", np.round(time.time() - timeSplit, 2))
 timeScale = time.time()
 # normalize features before training
 unscaled_x = np.vstack((posFeatures_arr, negFeatures_arr)).astype(np.float64)
-print(unscaled_x)
-print("Data fits to unscaled_x: ", np.shape(unscaled_x))
 scaler = StandardScaler().fit(unscaled_x) # normalize positive and negative features
 x = scaler.transform(unscaled_x) # x is the normalized values
 y = np.hstack((np.ones(totalvehicles), np.zeros(totalnonvehicles))) # y is an array of a tuple [1, 0]
@@ -72,30 +86,42 @@ accuracy = svc.score(x_test, y_test)
 print("Training time: ", np.round(time.time()-timeTrain, 2))
 print("Accuracy: ", np.round(accuracy, 4))
 
-eximage = cv2.imread("ford highway.jpg", cv2.COLOR_BGR2GRAY)
-bc = BinaryClassifier(svc, scaler)
-# use the slider to feed the classifier the window
-# define window width and height
-(winW, winH) = (64, 64)
 
-timeDetect = time.time()
-for (x, y, window) in Slider(eximage, stepSize=16, windowSize = (winW, winH)):
-    ex_features, ex_hog_image = hog(window, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True, multichannel = True)
-    arr = ex_features.reshape(1, -1)
-    if(arr.shape == (1, 128)):
-        if bc.predict(arr):
-            boxes.append((x, y, window))
-# draw boxes from the array over the image
-for (x, y, window) in boxes:
-    cv2.rectangle(eximage, (x,y),(x+winW, y+winH), (0,255,255), 2)
-print("Time for Detection: ", np.round(time.time()-timeDetect, 2))
-cv2.imshow('Example Image', eximage)
+cap = cv2.VideoCapture(0)
+while True:
+    boxes = []
+    output = []
+    check, frame = cap.read()
+    bc = BinaryClassifier(svc, scaler)
+    # use the slider to feed the classifier the window
+    # define window width and height
+    (winW, winH) = (64, 64)
 
+    timeDetect = time.time()
 
-##eximage = cv2.imread("./n/image1.png", cv2.COLOR_BGR2GRAY) # test with positive image
-##ex_features, ex_hog_image = hog(eximage, orientations=8, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=True, multichannel = True)
-##bc = BinaryClassifier(svc, scaler)
-##
-##arr = ex_features.reshape(1, -1)
-##
-##print(bc.predict(arr))
+### USE THREADING TO OPTIMIZE TIME
+    for (x, y, window) in Slider(frame, stepSize=16, windowSize = (winW, winH)):
+        task = delayed(predictFunc)(x, y, window, bc, boxes)
+        output.append(task)
+        #task = client.submit(predictFunc, (x, y, window, bc, boxes))
+
+    print('size of output: ', len(output)) # show that tasks are added
+    total = delayed(sum)(output)
+
+    total.compute()
+    print("size of boxes: ", len(boxes))
+    print("Time to detection: ", np.round(time.time()-timeDetect, 2))
+    # draw boxes from the array over the image
+    for (x, y, window, conf) in boxes:
+        label = "{:.2f}%".format(conf*100)
+        cv2.rectangle(frame, (x,y),(x+winW, y+winH), (0,255,255), 2)
+        cv2.putText(frame, label, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,255), 2)
+            
+    cv2.imshow('Camera Frame', frame)
+    key=cv2.waitKey(0)
+    if (key==27): # Esc key
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
+
